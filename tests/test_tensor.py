d339c2619b782590cb260df5aee40cc3cae8a21c
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-08-12 18:48:34 +0200

    test: Added new tensor tests.

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -7,3 +7,7 @@ def test_tensor_creation() -> None:
     x: Tensor = tensor([1, 2, 3])
     assert x.shape == (3,)
     assert x.dtype == float64
+
+    x: Tensor = tensor([[1, 2], [3, 4], [5, 6]])
+    assert x.shape == (3, 2)
+    assert x.dtype == float64
